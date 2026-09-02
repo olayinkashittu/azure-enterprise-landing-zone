67ee9d5ca0d7a0cc3e20f9ff28194e66fb428a6b
Olayinka Shittu <emailabc81@gmail.com>
2026-09-02 23:23:14 +0100

    Add production resource groups

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,42 @@ resource "azurerm_resource_group" "landing_zone" {
     Project     = var.project_name
   }
 }
+
+resource "azurerm_resource_group" "network" {
+  name     = "rg-${lower(var.project_name)}-${lower(var.environment)}-network"
+  location = var.location
+
+  tags = {
+    Environment = var.environment
+    Department  = var.department
+    Owner       = var.owner
+    CostCenter  = var.cost_center
+    Project     = var.project_name
+  }
+}
+
+resource "azurerm_resource_group" "application" {
+  name     = "rg-${lower(var.project_name)}-${lower(var.environment)}-app"
+  location = var.location
+
+  tags = {
+    Environment = var.environment
+    Department  = var.department
+    Owner       = var.owner
+    CostCenter  = var.cost_center
+    Project     = var.project_name
+  }
+}
+
+resource "azurerm_resource_group" "data" {
+  name     = "rg-${lower(var.project_name)}-${lower(var.environment)}-data"
+  location = var.location
+
+  tags = {
+    Environment = var.environment
+    Department  = var.department
+    Owner       = var.owner
+    CostCenter  = var.cost_center
+    Project     = var.project_name
+  }
+}
