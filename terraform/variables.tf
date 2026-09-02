193de03e9552dace1dbd791098d6e8e888696a4c
olayinka Shittu <98735445+olayinkashittu@users.noreply.github.com>
2026-09-02 19:50:07 +0100

    Add Terraform project variables
    Added variables for project configuration in Terraform.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,37 @@
+variable "project_name" {
+  description = "Name of the Azure Enterprise Landing Zone project"
+  type        = string
+  default     = "NovaSol"
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+  default     = "Production"
+}
+
+variable "location" {
+  description = "Azure region for resources"
+  type        = string
+  default     = "West Europe"
+}
+
+variable "owner" {
+  description = "Team responsible for the resources"
+  type        = string
+  default     = "CloudTeam"
+}
+
+variable "department" {
+  description = "Department responsible for the resources"
+  type        = string
+  default     = "IT"
+}
+
+variable "cost_center" {
+  description = "Cost center used for resource allocation"
+  type        = string
+  default     = "IT-001"
+}
+
 
