674ea00791e5a921ada9aade53d3e4a81ac2cb17
Olayinka Shittu <emailabc81@gmail.com>
2026-09-02 22:59:23 +0100

    Add landing zone resource group foundation

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,3 +12,16 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+resource "azurerm_resource_group" "landing_zone" {
+  name     = "rg-${lower(var.project_name)}-${lower(var.environment)}"
+  location = var.location
+
+  tags = {
+    Environment = var.environment
+    Department  = var.department
+    Owner       = var.owner
+    CostCenter  = var.cost_center
+    Project     = var.project_name
+  }
+}
