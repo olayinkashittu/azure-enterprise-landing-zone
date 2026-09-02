7c0929c60935201dc75f426765deafc412009346
Olayinka Shittu <emailabc81@gmail.com>
2026-09-02 23:34:18 +0100

    Refactor Terraform common tags

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,54 +13,36 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "landing_zone" {
-  name     = "rg-${lower(var.project_name)}-${lower(var.environment)}"
-  location = var.location
-
-  tags = {
+locals {
+  common_tags = {
     Environment = var.environment
     Department  = var.department
     Owner       = var.owner
     CostCenter  = var.cost_center
     Project     = var.project_name
   }
+}
+
+resource "azurerm_resource_group" "landing_zone" {
+  name     = "rg-${lower(var.project_name)}-${lower(var.environment)}"
+  location = var.location
+  tags     = local.common_tags
 }
 
 resource "azurerm_resource_group" "network" {
   name     = "rg-${lower(var.project_name)}-${lower(var.environment)}-network"
   location = var.location
-
-  tags = {
-    Environment = var.environment
-    Department  = var.department
-    Owner       = var.owner
-    CostCenter  = var.cost_center
-    Project     = var.project_name
-  }
+  tags     = local.common_tags
 }
 
 resource "azurerm_resource_group" "application" {
   name     = "rg-${lower(var.project_name)}-${lower(var.environment)}-app"
   location = var.location
-
-  tags = {
-    Environment = var.environment
-    Department  = var.department
-    Owner       = var.owner
-    CostCenter  = var.cost_center
-    Project     = var.project_name
-  }
+  tags     = local.common_tags
 }
 
 resource "azurerm_resource_group" "data" {
   name     = "rg-${lower(var.project_name)}-${lower(var.environment)}-data"
   location = var.location
-
-  tags = {
-    Environment = var.environment
-    Department  = var.department
-    Owner       = var.owner
-    CostCenter  = var.cost_center
-    Project     = var.project_name
-  }
+  tags     = local.common_tags
 }
