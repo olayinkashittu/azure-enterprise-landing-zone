d6f38e5caa689f617bd2f581203719f6e5043e14
Olayinka Shittu <emailabc81@gmail.com>
2026-09-03 09:25:18 +0100

    Add resource group outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -17,3 +17,23 @@ output "owner" {
   description = "Team responsible for the resources"
   value       = var.owner
 }
+
+output "landing_zone_resource_group" {
+  description = "Name of the primary landing zone resource group"
+  value       = azurerm_resource_group.landing_zone.name
+}
+
+output "network_resource_group" {
+  description = "Name of the network resource group"
+  value       = azurerm_resource_group.network.name
+}
+
+output "application_resource_group" {
+  description = "Name of the application resource group"
+  value       = azurerm_resource_group.application.name
+}
+
+output "data_resource_group" {
+  description = "Name of the data resource group"
+  value       = azurerm_resource_group.data.name
+}
